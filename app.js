a54abe448999acd9e471a72bd4d1afe7b9f91a36
eswat2 <eswat2@gmail.com>
2016-08-11 18:56:22 -0700

    add all the params for search

--- app.js
+++ app.js
@@ -13,13 +13,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-let getOptions = (key, q) => {
+let searchOptions = (key, q, sort, page) => {
   let opts = {
     method: 'GET',
     url: 'http://food2fork.com/api/search',
     qs: {
-      key,
-      q
+      key
     },
     headers: {
       'cache-control': 'no-cache',
@@ -27,13 +26,60 @@ let getOptions = (key, q) => {
     },
     json: true
   }
+
+  if (q) {
+    opts.qs.q = q;
+  }
+
+  if (sort) {
+    opts.qs.sort = sort;
+  }
+
+  if (page) {
+    opts.qs.page = page;
+  }
+
   return opts;
 };
 
+//
+// NOTE:  search API
+//
+//  key: API Key
+//    q: (optional) Search Query (Ingredients should be separated by commas).
+//                  If this is omitted top rated recipes will be returned.
+// sort: (optional) How the results should be sorted. See Below for details.
+// page: (optional) Used to get additional results
+//
+// -----------------------------------------------------------------------
+// Search Sorting
+//
+// The Food2Fork API offers two kinds of sorting for queries. The first is
+// by rating. This rating is based off of social media scores to determine
+// the best recipes.
+//
+// sort=r
+//
+// The second is by trendingness. The most recent recipes from our
+// publishers have a trend score based on how quickly they are gaining
+// popularity.
+//
+// sort=t
+//
+// -----------------------------------------------------------------------
+// Pages (Search Only)
+//
+// Any request will return a maximum of 30 results. To get the next set
+// of results send the same request again but with page = 2
+// The default if omitted is page = 1
+//
+//
 app.post('/search', function (req, res, next) {
-  let key = req.body.key;
-  let q   = req.body.q;
-  let opt = getOptions(key, q);
+  let key  = req.body.key;
+  let q    = req.body.q;
+  let sort = req.body.sort;
+  let page = req.body.page;
+  let opt  = searchOptions(key, q, sort, page);
 
   request(opt, function (error, response, body) {
     if (error) {
